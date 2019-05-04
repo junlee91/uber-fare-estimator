8aba31fc7988081c096dc12d95f440fd40c6ab11
junlee91 <jlee757@uic.edu>
2019-05-04 00:12:10 -0500

    Icons error fix

--- src/Components/Icons.js
+++ src/Components/Icons.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export const UberGlobeIcon = () => (
-  <svg width="1em" height="1em" viewBox="0 0 24 24" fill="none" class="test">
+  <svg width="1em" height="1em" viewBox="0 0 24 24" fill="none">
     <title>EN</title>
     <path
       d="M12 1C5.9 1 1 5.9 1 12s4.9 11 11 11 11-4.9 11-11S18.1 1 12 1zm8 11c0 .7-.1 1.4-.3 2-.6-1.5-1.6-3.1-3-4.7l1.8-1.8c1 1.3 1.5 2.8 1.5 4.5zM6.5 6.5c1.3 0 3.6.8 6 2.9l-3.2 3.2C7.1 9.8 6.5 7.5 6.5 6.5zm8.1 5c2.3 2.7 2.9 5 2.9 6-1.3 0-3.6-.8-6-2.9l3.1-3.1zm1.9-6.1l-1.9 1.9c-1.6-1.4-3.2-2.4-4.7-3 .7-.2 1.3-.3 2-.3 1.8 0 3.3.5 4.6 1.4zM4 12c0-.7.1-1.4.3-2 .6 1.5 1.6 3.1 3 4.7l-1.8 1.8C4.5 15.2 4 13.7 4 12zm3.5 6.6l1.9-1.9c1.6 1.4 3.2 2.4 4.7 3-.7.2-1.3.3-2 .3-1.8 0-3.3-.5-4.6-1.4z"
@@ -14,8 +14,8 @@ export const UberRewardsIcon = () => (
   <svg width="1em" height="1em" viewBox="0 0 24 24" fill="none">
     <title>Rewards</title>
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M19.3 12L12 4.7 4.7 12H12v7.3l7.3-7.3zM.5 12L12 .5 23.5 12 12 23.5.5 12z"
       fill="white"
     />
@@ -26,8 +26,8 @@ export const UberPayIcon = () => (
   <svg width="1em" height="1em" viewBox="0 0 24 24" fill="none">
     <title>Pay</title>
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M1 20V4h22v16H1zm16-6h3V7H7v3H4v7h13v-3zm-2-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"
       fill="white"
     />
@@ -38,8 +38,8 @@ export const UberLoginIcon = () => (
   <svg width="1em" height="1em" viewBox="0 0 24 24" fill="none">
     <title>Log in</title>
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M17.5 6.5c0 3-2.5 5.5-5.5 5.5S6.5 9.5 6.5 6.5 9 1 12 1s5.5 2.5 5.5 5.5zm-3 0C14.5 5.1 13.4 4 12 4S9.5 5.1 9.5 6.5 10.6 9 12 9s2.5-1.1 2.5-2.5zM3 20c0-3.3 2.7-6 6-6h6c3.3 0 6 2.7 6 6v3h-3v-3c0-1.7-1.4-3-3-3H9c-1.6 0-3 1.3-3 3v3H3v-3z"
       fill="white"
     />
