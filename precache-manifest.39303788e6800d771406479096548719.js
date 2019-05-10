f5c41e964322fbc496234985fdc19920b13736d1
junlee91 <jlee757@uic.edu>
2019-05-09 22:35:26 -0500

    Updates

---
+++ precache-manifest.39303788e6800d771406479096548719.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36cc91113db9d50998b45bf63240f5f8",
+    "url": "/uber-fare-estimator/index.html"
+  },
+  {
+    "revision": "062d66e1a1f92e8dd98c",
+    "url": "/uber-fare-estimator/static/js/2.944d79ae.chunk.js"
+  },
+  {
+    "revision": "7c4d81c10b63f40905a2",
+    "url": "/uber-fare-estimator/static/js/main.4d9bc862.chunk.js"
+  },
+  {
+    "revision": "f4889c09948764a2c593",
+    "url": "/uber-fare-estimator/static/js/runtime~main.9476b450.js"
+  }
+]);
